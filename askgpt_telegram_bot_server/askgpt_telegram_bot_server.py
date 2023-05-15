10a1fcf1edaf13b40948f41e140fca1645dfb9b8
tlkahn <joshgy@gmail.com>
2023-05-15 21:07:25 +0800

    First commit

---
+++ askgpt_telegram_bot_server/askgpt_telegram_bot_server.py
@@ -0,0 +1,39 @@
+import logging
+import os
+
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
+
+load_dotenv()
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+
+async def ask(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.effective_chat:
+        if incoming_msg := update.effective_message:
+            query = incoming_msg.text
+
+            await incoming_msg.reply_text(
+                f"Hi! you just said {incoming_msg.text}. Awesome!!"
+            )
+
+
+def main():
+    TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+    if TELEGRAM_BOT_TOKEN is None:
+        print("TELEGRAM_BOT_TOKEN not found")
+        return
+    telegram_bot = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+    start_handler = CommandHandler("ask", ask)
+    telegram_bot.add_handler(start_handler)
+    telegram_bot.run_polling()
